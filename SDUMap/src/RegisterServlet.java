66663f8c5357a05c67d21a985b7bcf6f8096f5a4
YIWEI <f1f3f@qq.com>
2017-04-27 17:16:29 +0800

    add Login action

--- SDUMap/src/RegisterServlet.java
+++ SDUMap/src/RegisterServlet.java
@@ -1,24 +1,18 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.jms.Session;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-public class LoginServlet extends HttpServlet {
+public class RegisterServlet extends HttpServlet {
 
-	private String username = null;
-	private String password = null;
-	
-	
 	/**
 	 * Constructor of the object.
 	 */
-	public LoginServlet() {
+	public RegisterServlet() {
 		super();
 	}
 
@@ -43,7 +37,19 @@ public class LoginServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		doPost(request,response);
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+		out.println("<HTML>");
+		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+		out.println("  <BODY>");
+		out.print("    This is ");
+		out.print(this.getClass());
+		out.println(", using the GET method");
+		out.println("  </BODY>");
+		out.println("</HTML>");
+		out.flush();
+		out.close();
 	}
 
 	/**
@@ -58,30 +64,20 @@ public class LoginServlet extends HttpServlet {
 	 */
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		username = request.getParameter("username");
-		password = request.getParameter("password");
-			
-		try {
-			if (username == "" || password == "") {
-				System.out.println("kong");
-				RequestDispatcher rd = request
-						.getRequestDispatcher("../Login.html");
-				response.sendRedirect("../Login.html");
-				return;
-			}
 
-			boolean isValid = false;
-			UserBean userBean = new UserBean();
-			isValid = userBean.valid(username, password);
-			System.out.println(username + password);
-			if (isValid) {
-				response.sendRedirect("../index.jsp");
-			} else {
-				response.sendRedirect("../Login.html");
-			}
-		} catch (Exception e) {
-
-		}
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+		out.println("<HTML>");
+		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+		out.println("  <BODY>");
+		out.print("    This is ");
+		out.print(this.getClass());
+		out.println(", using the POST method");
+		out.println("  </BODY>");
+		out.println("</HTML>");
+		out.flush();
+		out.close();
 	}
 
 	/**
